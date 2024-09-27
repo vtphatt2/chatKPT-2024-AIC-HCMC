9185e5e892de51196f9e0a0c69752c6c0d4d1577
vtphatt2 <thinhphat544@gmail.com>
2024-09-27 18:48:33 +0700

    ready for round 3

--- src/static/temporal_search.js
+++ src/static/temporal_search.js
@@ -54,7 +54,7 @@ function performTemporalSearch() {
                 const imgElement = document.createElement('img');
                 imgElement.src = `/image/${imagePath.substring(1)}`;  // Serve the image via the /image/<path>
                 imgElement.alt = `Frame ${frameId}`;
-                imgElement.style.width = '150px';  // Set initial width
+                imgElement.style.width = '300px';  // Set initial width
                 imgElement.style.height = 'auto';   // Set initial height
                 imgElement.onclick = function() { toggleZoom(imgElement); };
 
