41f4e5bb06848b5aaeddfe152b04a20f1eae3ebc
vtphatt2 <thinhphat544@gmail.com>
2024-10-14 12:11:59 +0700

    finish search by transcript

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,6 @@
+import unicodedata
+import re
+
 def time_difference(start: str, end: str) -> int:
     def convert_to_seconds(time_str: str) -> int:
         minutes, seconds = map(int, time_str.split(':'))
@@ -53,4 +56,28 @@ def concatenate_surrounding_strings(tuples_list, frame_id, fps):
     
     return ' '.join(strings_to_concatenate)
 
+def normalize_text(text):
+    text = unicodedata.normalize('NFD', text)
+    text = ''.join([char for char in text if not unicodedata.combining(char)])
+    text = text.lower()
+    return text
+
+def count_keywords_in_string(keywords, text):
+    normalized_text = normalize_text(text)
+    total_count = 0
+    for keyword in keywords:
+        normalized_keyword = normalize_text(keyword)
+        matches = re.findall(re.escape(normalized_keyword), normalized_text)
+        total_count += len(matches)
+    return total_count
+
+def find_closest_frame_index(samples, target_frame_id):
+    min_diff = float('inf')
+    min_index = -1
+    for i, sample in enumerate(samples):
+        diff = abs(sample['frame_id'] - target_frame_id)
+        if diff < min_diff:
+            min_diff = diff
+            min_index = i
+    return min_index
 
