69759a8ee9a170c2966279659aa03b93a172d01a
vtphatt2 <thinhphat544@gmail.com>
2024-10-19 11:54:12 +0700

    Remove push_data.sh from repository and add to .gitignore

--- push_data.sh
+++
@@ -1,7 +0,0 @@
-sshpass -p "kbyvWCLI8098" rsync -a -P /Users/VoThinhPhat/Desktop/chatKPT-2024-AIC-HCMC/data/batch3/keyframes/keyframes_L25_a root@103.20.97.117:/root/khang_ly/batch3/keyframes
-sshpass -p "kbyvWCLI8098" rsync -a -P /Users/VoThinhPhat/Desktop/chatKPT-2024-AIC-HCMC/data/batch3/keyframes/keyframes_L26_a root@103.20.97.117:/root/khang_ly/batch3/keyframes
-sshpass -p "kbyvWCLI8098" rsync -a -P /Users/VoThinhPhat/Desktop/chatKPT-2024-AIC-HCMC/data/batch3/keyframes/keyframes_L26_b root@103.20.97.117:/root/khang_ly/batch3/keyframes
-sshpass -p "kbyvWCLI8098" rsync -a -P /Users/VoThinhPhat/Desktop/chatKPT-2024-AIC-HCMC/data/batch3/keyframes/keyframes_L26_c root@103.20.97.117:/root/khang_ly/batch3/keyframes
-sshpass -p "kbyvWCLI8098" rsync -a -P /Users/VoThinhPhat/Desktop/chatKPT-2024-AIC-HCMC/data/batch3/keyframes/keyframes_L26_d root@103.20.97.117:/root/khang_ly/batch3/keyframes
-sshpass -p "kbyvWCLI8098" rsync -a -P /Users/VoThinhPhat/Desktop/chatKPT-2024-AIC-HCMC/data/batch3/keyframes/keyframes_L26_e root@103.20.97.117:/root/khang_ly/batch3/keyframes
-sshpass -p "kbyvWCLI8098" rsync -a -P /Users/VoThinhPhat/Desktop/chatKPT-2024-AIC-HCMC/data/batch3/keyframes/keyframes_L27_a root@103.20.97.117:/root/khang_ly/batch3/keyframes
